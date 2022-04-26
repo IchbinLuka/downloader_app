5b48e320d8feca2d9eb1ce9cfff418d5d8e99029
Luka Born <44705673+IchbinLuka@users.noreply.github.com>
2022-04-26 22:59:52 +0200

    Added more supported platforms

--- app/src/main/java/com/ichbinluka/downloader/data/SupportedPlatforms.kt
+++ app/src/main/java/com/ichbinluka/downloader/data/SupportedPlatforms.kt
@@ -2,11 +2,13 @@ package com.ichbinluka.downloader.data
 
 import java.util.regex.Pattern
 
-const val httpsRegex = "https://|http://"
+const val httpsRegex = "(https://|http://)"
 
 val supportedPlatforms = listOf(
     "${httpsRegex}youtube.com(.*)",
     "(.*)youtu.be(.*)",
-    "https://www.reddit(.*)",
-    "https://redd.it(.*)"
+    "${httpsRegex}www.reddit(.*)",
+    "${httpsRegex}redd.it(.*)",
+    "${httpsRegex}(www.)?tiktok.com(.*)",
+    "${httpsRegex}(www.)?twitter.com(.*)"
 ).map { Regex(it) }
